e010d3245e60c3dd12bbf30b02455eb7b36a041e
Punyapat Lertsamrong <punyapat1412@gmail.com>
2024-06-27 14:11:03 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router';
+</script>
+
+<template>
+  <header>
+    <div>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+  <hr />
+  <RouterView />
+</template>
